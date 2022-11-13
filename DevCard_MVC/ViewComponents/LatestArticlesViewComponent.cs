d4129847e7295c3bbbf24d46412e132f204d7fd5
iLiYa <iLiYa@DESKTOP-1NC4T8B>
2022-11-13 12:46:36 +0330

    Projects & Latest Articles Moved to Component

---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var article = new List<Article>
+            {
+                new Article(1, "آموزش ASP.NET Core mvc", "کاملترین پکیچ برنامه نویسی ASP.Net core mvc  به زبان فارسی",
+                    "blog-post-thumb-card-1.jpg"),
+                new Article(2, "آموزش Git & Github", "کاملترین پکیچ برنامه نویسی Git & Github  به زبان فارسی",
+                    "blog-post-thumb-card-2.jpg"),
+                new Article(3, "آموزش Onion Architecture",
+                    "کاملترین پکیچ برنامه نویسی Onion Architecture  به زبان فارسی",
+                    "blog-post-thumb-card-3.jpg"),
+                new Article(4, "آموزش طراحی سایت", "کاملترین پکیچ برنامه نویسی طراحی سایت  به زبان فارسی",
+                    "blog-post-thumb-card-4.jpg")
+            };
+            return View("_LatestArticles", article);
+        }
+    }
+}
---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+	public class ProjectsViewComponent : ViewComponent 
+	{
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>()
+            {
+                new Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفرهای شهری", "project-1.jpg", "Atriya" ),
+                new Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور", "project-2.jpg","ZoodFood" ),
+                new Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "project-3.jpg","MONOP" ),
+                new Project(4, "فضاپیما", "برنامه مدیریت فضاهای ناسا", "project-4.jpg","NASA" ),
+            };
+            return View("_Projects", projects);
+        }
+	}
+}
